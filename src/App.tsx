2c74764cdd1a3042a905e438a032c689774e746a
Jennifer Proust <jennifer.proust@gmail.com>
2020-11-25 00:52:12 +0100

    feat(routing): add a router and create Home page component
    Also add a temporary 404 display
    
    fix #20

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { CssBaseline } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Container, CssBaseline, Paper, Typography } from "@material-ui/core";
 import { ThemeProvider } from '@material-ui/core/styles';
+import { Home } from "./pages/Home";
 import themes from './themes';
 import './i18n';
 import { useTranslation } from "react-i18next";
@@ -12,10 +14,24 @@ const selectedTheme = themes.Brownwave;
 const App = () => {
   const { t } = useTranslation();
   return (
-    <ThemeProvider theme={selectedTheme}>
-      <CssBaseline />
-      <main>{t('hello')}</main>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={selectedTheme}>
+        <CssBaseline />
+        <Switch>
+          <Route path="/" exact={true} children={<Home />} />
+          <Route path="*">
+            <Container>
+              <Paper>
+                <Typography variant="h1" align="center">
+                  404
+                </Typography>
+                <Link to="/">{t('go-back-home')}</Link>
+              </Paper>
+            </Container>
+          </Route>
+        </Switch>
+      </ThemeProvider>
+    </Router>
   );
 };
 
