5648ae6b46ded0ed76ae9e192e7bb3b85329b41f
Jennifer Proust <jennifer.proust@gmail.com>
2020-11-24 22:06:22 +0100

    feat(messageslist): create MessagesList component
    fix #19

---
+++ src/fixtures/messages.ts
@@ -0,0 +1,21 @@
+import { MessageProps } from "../components/Message";
+
+export const messages: MessageProps[] = [
+  {
+    id: '111',
+    title: 'title 1',
+    body: 'body 1',
+    isPrivate: true,
+  },
+  {
+    id: '222',
+    title: 'title 2',
+    body: 'body 2',
+    isPrivate: false,
+  },
+  {
+    id: '333',
+    title: 'title 3',
+    body: 'body 3',
+  },
+];
